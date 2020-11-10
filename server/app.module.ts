987d0dfc160e2f375a7e7a28026a17d07a804edc
Felix Xu <felix.xu.mail@gmail.com>
2020-11-10 10:06:08 +0800

    feat: add mongodb

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,12 +1,13 @@
 import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { config } from '../config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NuxtController } from './controller/nuxt.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config.database)],
+  imports: [TypeOrmModule.forRoot(config.database), MongooseModule.forRoot(config.mongoDB.uri)],
   controllers: [AppController, NuxtController],
   providers: [AppService, Logger],
 })
