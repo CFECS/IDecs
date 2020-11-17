a8ba4787089c620073eda8a8b0b55ac6cdb030bb
felix-xuu <felix-xuu@users.noreply.github.com>
2020-11-17 15:26:48 +0000

    deploy: ff86c8b7002ff2566317f8c014162934e93db8bb

---
+++ _nuxt/static/1605626802/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", (function(a,b,c){return {data:[{document:{slug:"index",title:a,description:a,category:"快速上手",menuTitle:"认识IDecs",position:1,toc:[],body:{type:"root",children:[]},dir:"\u002Fzh-CN",path:"\u002Fzh-CN\u002Findex",extension:".md",createdAt:b,updatedAt:b,to:"\u002F"},prev:c,next:c}],fetch:[],mutations:[]}}("IDecs简介","2020-11-17T15:24:29.322Z",null)));
