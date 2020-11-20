4f06861997ae6ca819b76fbadb313cce639e5ddd
Felix Xu <felix.xu.mail@gmail.com>
2020-11-20 09:24:13 +0800

    refactor(dependencies): remove circle DI

--- server/module/global.module.ts
+++ server/module/global.module.ts
@@ -1,9 +1,12 @@
 import { Global, Logger, Module } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from '../model/rds/user.model';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([UserModel])],
   providers: [Repository, Logger],
-  exports: [Repository, Logger],
+  exports: [Repository, Logger, TypeOrmModule],
 })
 export class GlobalModule {}
