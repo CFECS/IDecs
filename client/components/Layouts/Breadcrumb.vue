102a4498a32476d1421a7b9863e9086c083e2e89
ziping-li <73812217+ziping-li@users.noreply.github.com>
2020-11-25 14:36:07 +0800

    feat: add breadcrumb component

--- client/components/Layouts/Breadcrumb.vue
+++ client/components/Layouts/Breadcrumb.vue
@@ -1,7 +1,14 @@
 <template>
   <a-breadcrumb class="section no-cursor">
-    <a-breadcrumb-item v-for="item in routes" :key="item.name">
-      {{ item.name }}
+    <a-breadcrumb-item v-for="(item, index) in routes" :key="item.name">
+      <nuxt-link v-if="index !== routes.length - 1" :to="item.path">
+        <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
+        {{ $t(item.meta.title) }}
+      </nuxt-link>
+      <template v-else>
+        <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
+        {{ $t(item.meta.title) }}
+      </template>
     </a-breadcrumb-item>
   </a-breadcrumb>
 </template>
@@ -12,7 +19,12 @@ import { Component, Vue } from 'nuxt-property-decorator';
 @Component
 export default class Breadcrumb extends Vue {
   get routes() {
-    return this.$route.matched;
+    const matched: any = [...this.$route.matched];
+    if (matched.length === 1 && matched[0].name !== 'index') {
+      matched.unshift({ name: 'index', path: '/', meta: { title: 'COMMON.PAGE_TITLE.INDEX', icon: 'home' } });
+    }
+    console.log(matched);
+    return matched;
   }
 }
 </script>
